de9fe52f178350091e5dbab908cd3bcfb3b91899
calumrodger <calumrodger@gmail.com>
2023-03-27 11:58:21 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,20 @@
+if (!process.env.WORDPRESS_API_URL) {
+  throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables WORDPRESS_API_URL.
+  `)
+}
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    domains: [
+      process.env.WORDPRESS_API_URL.match()[0], // Valid WP Image domain.
+      '0.gravatar.com',
+      '1.gravatar.com',
+      '2.gravatar.com',
+      'secure.gravatar.com',
+      'wp-cms.calumrodger.co.uk'
+    ],
+  },
+}
