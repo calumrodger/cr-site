788db04049571371e73801875842f99256b12495
calumrodger <calumrodger@gmail.com>
2023-04-01 14:13:49 +0100

    blurb innerhtml

--- components/post-preview-featured.js
+++ components/post-preview-featured.js
@@ -10,7 +10,7 @@ const PostPreviewFeatured = (props) => {
         <img className={classes.image} src={props.image} alt="alt text" />
         <p className={classes.tags}>Tags: {props.tags.map((tag, i, arr) => <Link key={i} href={`/tag/${tag}`}><span className={classes.tag}>{tag}</span>{i != (arr.length - 1) ? ', ' : ''}</Link>)}</p>
         {/* <div className={classes.content} dangerouslySetInnerHTML={{__html: props.content}}/>   */}
-        <p className={classes.blurb}>{props.blurb}</p> 
+        <p className={classes.blurb} dangerouslySetInnerHTML={{__html: props.blurb}}/> 
         </div>
     )
 }
--- components/post-preview.js
+++ components/post-preview.js
@@ -11,7 +11,7 @@ const PostPreview = (props) => {
         <img className={classes.image} src={props.image} alt="alt text"/>
         <p className={classes.tags}>Tags: {props.tags.map((tag, i, arr) => <Link key={i} href={`/tag/${tag}`}><span className={classes.tag}>{tag}</span>{i != (arr.length - 1) ? ', ' : ''}</Link>)}</p>
         {/* <div className={classes.content} dangerouslySetInnerHTML={{__html: props.content}}/>    */}
-        <div className={classes.blurb}>{props.blurb}</div>
+        <div className={classes.blurb} dangerouslySetInnerHTML={{__html: props.blurb}}/>
         </div>
         </>
     )
