688cb7647c504b8b758e23d77482e867ede79585
calumrodger <calumrodger@gmail.com>
2023-04-08 20:10:48 +0100

    switch off live site

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,26 @@ import '../styles/globals.scss'
 import NavBar from '../components/nav-bar'
 import Footer from '../components/footer'
 import PostPreviewFeatured from '../components/post-preview-featured'
+import { useState } from 'react'
 
 export default function App({ Component, pageProps }) {
 
   const { posts } = pageProps
   const { randomPost } = pageProps
   const { categories } = pageProps
+
+  const [access, setAccess] = useState(false)
   
   return (
         <>
+        { access ?
         <div className='global-container'>
       <NavBar posts={posts} randomPost={randomPost} categories={categories}/>
   <Component {...pageProps} />
   <Footer />
   </div>
+  : 
+  <p>underconstruction</p>}
     </>
   )
 }
