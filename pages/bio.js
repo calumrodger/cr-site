a2de2121070c327ce5c051721a6abc2f92f98d2e
calumrodger <calumrodger@gmail.com>
2023-04-29 17:26:49 +0100

    coming along

---
+++ pages/bio.js
@@ -0,0 +1,40 @@
+import { getPostData, postDataSorter, getRandomPost, categoryDataSorter, getCategoryData } from "../helpers/api-utils";
+import classes from './index.module.scss'
+
+const BioPage = (props) => {
+
+  const { posts } = props
+
+  let bioPageContent = posts.find(item => item.slug === 'bio')
+
+  if (!bioPageContent) {
+    bioPageContent = ''
+  }
+
+  return (
+    <>
+    <div className={classes.pageContainer}>
+      <div className={classes.pageContent}>
+        <div className={classes.content} dangerouslySetInnerHTML={{__html: bioPageContent.content}}/>
+      </div>
+    </div>
+    </>
+  )
+}
+
+
+export async function getStaticProps() {
+  const data = await getPostData()
+  const posts = postDataSorter(data)
+  const categoryData = await getCategoryData()
+  const categories = categoryDataSorter(categoryData)
+  const randomPost = await getRandomPost(posts)
+
+  return {
+    props: { posts, randomPost, categories },
+    revalidate: 600
+  }
+}
+
+
+export default BioPage;
