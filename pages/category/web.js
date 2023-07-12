45941a952549d3a980a22d3cb2d4058c44b7f4c0
calumrodger <calumrodger@gmail.com>
2023-07-12 16:10:59 +0100

    .

--- pages/category/web.js
+++ pages/category/web.js
@@ -18,13 +18,13 @@ const WebCategory = (props) => {
         <div className={classes.poemsListContainer}>
         <h3>Original Poems</h3>
         <ul className={classes.poemsList}>
-        <li><Link href='/poem/spring-poem'>spring poem</Link></li>
+        <li><Link href='/poem/spring-poem'>spring poem</Link> (static)</li>
         </ul>
         <h3>Cover Versions</h3>
         <ul className={classes.poemsList}>
-        <li><Link href='/poem/silence'>silence</Link></li>
+        <li><Link href='/poem/silence'>silence</Link> (interactive)</li>
         <p>Interactive remake of one of the earliest concrete poems, Eugen Gomringer&apos;s &apos;silence&apos; (1954). To play: touch the silence.</p>
-        <li><Link href='/poem/the-lighght-going-on-and-off'>{lighghtTitle}</Link></li>
+        <li><Link href='/poem/the-lighght-going-on-and-off'>{lighghtTitle}</Link> (animated)</li>
         <p>Animated homage to Aram Saroyan&apos;s iconic minimalist poem &apos;lighght&apos; (1966), by way of Martin Creed&apos;s Turner Prize-winning artwork &apos;Work No. 227: The Lights Going On and Off&apos; (2001). </p>
         </ul>
         </div>
