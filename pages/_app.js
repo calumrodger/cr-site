67fbc1fc330fbacf2a0b5192f373e20b734dbd37
calumrodger <calumrodger@gmail.com>
2023-07-13 22:51:12 +0100

    trying ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import classes from '../styles/globals.scss'
 import Layout from '../components/layout'
-import { useEffect } from 'react';
+import Script from "next/script"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import * as gtag from "../helpers/gtag"
 
 export default function App({ Component, pageProps }) {
-
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
 
   const { posts } = pageProps
   const { categories } = pageProps
@@ -11,6 +23,24 @@ export default function App({ Component, pageProps }) {
   
   return (
     <>
+    <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
           <Layout posts={posts} categories={categories} cat={cat}>
           <Component {...pageProps} />
           </Layout>
