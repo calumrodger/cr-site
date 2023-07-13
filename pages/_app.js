87832d85188472a863daf6e6e5ac8d330cfb9e66
calumrodger <calumrodger@gmail.com>
2023-07-13 23:06:22 +0100

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
+import classes from '../styles/globals.scss'
 import Layout from '../components/layout'
 import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
+  
 
   const { posts } = pageProps
   const { categories } = pageProps
@@ -9,10 +11,12 @@ export default function App({ Component, pageProps }) {
   
   return (
     <>
+  
           <Layout posts={posts} categories={categories} cat={cat}>
           <Component {...pageProps} />
           </Layout>
           <Analytics />
+          
     </>
   )
 }
