96622ef9ae1cdcabc22c808023e925a691b8a779
calumrodger <calumrodger@gmail.com>
2023-07-18 21:47:47 +0100

    .

--- pages/poem/among.js
+++ pages/poem/among.js
@@ -6,10 +6,16 @@ const FoliageComponent = () => {
   return (
     <div className={classes.pageContent}>
     <div className={classes.foliageContainer}>
-      <div className={classes.foliageText}>
+        <div className={classes.foliageText}>
+      <div className={classes.foliageText1}>
         the foliage of the park <br />
+        </div>
+        <div className={classes.foliageText2}>
         among <br />
+        </div>
+        <div className={classes.foliageText3}>
         the foliage of the room
+        </div>
       </div>
     </div>
     </div>
