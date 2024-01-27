276a6e0c2b4932648742a32591964dd96ea88e56
calumrodger <calumrodger@gmail.com>
2024-01-27 18:02:55 +0000

    different font import

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,13 +4,13 @@ import localFont from 'next/font/local'
  
 // Font files can be colocated inside of `app`
 const myFont = localFont({
-  src: './fonts/LeagueSpartan-VF.ttf',
+  src: '../../public/fonts/LeagueSpartan-VF.ttf',
   display: 'swap',
 })
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={myFont.spartan}>
+    <html lang="en" className={myFont.className}>
       <body>{children}</body>
     </html>
   )
