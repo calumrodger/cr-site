f34cf98a906d34b2bd45381645674caef194eb0f
calumrodger <calumrodger@gmail.com>
2024-01-29 21:48:45 +0000

    font change

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,20 @@
 import localFont from 'next/font/local'
  
 // Font files can be colocated inside of `app`
+import { Lexend } from 'next/font/google'
+
+const lexend = Lexend({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
 const myFont = localFont({src: '../../public/fonts/LeagueSpartan-VF.ttf'})
 
 import '../styles/globals.scss'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={myFont.className}>
+    <html lang="en" className={lexend.className}>
       <body>{children}</body>
     </html>
   )
