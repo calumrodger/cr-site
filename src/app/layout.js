7dbae35cdc1be69b6d86f0134d315af764d4c17b
calumrodger <calumrodger@gmail.com>
2024-02-20 17:15:33 +0000

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 // Font files can be colocated inside of `app`
 import { lexend } from './fonts.js'
+import { Analytics } from '@vercel/analytics/react';
 
 import '../styles/globals.scss'
 
@@ -7,6 +8,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={lexend.className}>{children}</body>
+      <Analytics />
     </html>
   )
 }
