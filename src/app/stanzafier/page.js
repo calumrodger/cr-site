fc678abc9a849d766b5a95b624cd6229e00d6009
calumrodger <calumrodger@gmail.com>
2024-06-11 22:17:09 +0100

    .

--- src/app/stanzafier/page.js
+++ src/app/stanzafier/page.js
@@ -9,7 +9,7 @@ export default async function PoemPage() {
 
   // const source = "This is the STANZA PAD. \n Click on a word to SELECT it. \n Transform SELECTED words with the WORD BANK and FX panels. \n When your stanza is ready, click SAVE STANZA. \n To create a new stanza, click GENERATE. \n Click POEM PAD to view and rearrange stanzas. \n Click OUTPUT to admire your poem. \n Click HELP/DOCS to learn more.";
 
-  const source = "This: the STANZA PAD. \n Click a word to SELECT it. \n Play with the buttons.";
+  const source = "This, the STANZA PAD: \n click a word to SELECT it; \n play with the buttons.";
   return (
           <Genny source={source}/>
   )
