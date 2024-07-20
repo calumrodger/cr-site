117f632f2ebe297f1e6d7ba86c6813133db013e0
calumrodger <calumrodger@gmail.com>
2024-07-20 11:09:43 +0100

    .

---
+++ src/tg/word-bank/add-words.js
@@ -0,0 +1,11 @@
+import classes from '../tg-styles.module.scss';
+
+const AddCustomWordsToWordBank = () => {
+    return (
+        <div>
+            AddCustomWordsToWordBank
+        </div>
+    );
+}
+
+export default AddCustomWordsToWordBank;
